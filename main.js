d85053bce981d26869eb87ee22bcf574a6814d61
manickaa <52226049+manickaa@users.noreply.github.com>
2019-11-18 20:26:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -34,7 +33,7 @@ app.use(express.static('public'));
   });
 }*/
 
-function getCustomer(res, mysql, context, done){
+function getCustomer(res, mysql, context, complete){
   var sql_query = "SELECT customers.customer_ID as customer_ID, customers.first_name as first_name, customers.middle_name as middle_name, customers.last_name as last_name,customers.street_no as street_no, customers.city as city, customers.state as state, customers.country as country,customers.phone_number as phone_number, customers.email_id as email_id, COUNT(bookings.booking_ID) AS numberofbookings,COUNT(ratings.rating_ID) AS numberofreviews FROM customers LEFT JOIN bookings ON customers.customer_ID = bookings.customer_ID LEFT JOIN ratings ON customers.customer_ID = ratings.rating_ID GROUP BY customers.customer_ID ORDER BY customers.customer_ID;";
   mysql.pool.query(sql_query, function(err, result, fields){
       if(err){
@@ -61,6 +60,56 @@ function getBooking(res, mysql, context, complete){
       complete();
   });
 }
+/*Search & return all location*/
+function getLocationsDetails(req, res, mysql, context, complete) {
+     var sql = ("SELECT travel_location.travelLocation_ID AS 'location_ID', travel_location.city AS 'city', travel_location.country as 'country'," +
+                    "COUNT(bookings.booking_ID) AS 'count_bookings', COUNT(tour_guide.tourGuide_ID) AS 'count_guides'," +
+                    "SUM(number_adults) + SUM(number_children) AS 'adults_kids'," +
+                    "ROUND(AVG(ratings.rating),2) AS 'ave_review'" +
+                    "FROM travel_location " +
+                    "LEFT JOIN bookings ON travel_location.travelLocation_ID = bookings.travelLocation_ID" +
+                    "LEFT JOIN assignment ON bookings.booking_ID = assignment.booking_ID" +
+                    "LEFT JOIN tour_guide ON assignment.tourGuide_ID = tour_guide.tourGuide_ID" +
+                    "LEFT JOIN customers ON bookings.customer_ID = customers.customer_ID" +
+                    "LEFT JOIN ratings ON customers.customer_ID = ratings.customer_ID" +
+                    "WHERE travel_location.travelLocation_ID = ISNULL(?,travel_location.travelLocation_ID)" +
+                    "GROUP BY travel_location.travelLocation_ID" +
+                    "ORDER BY travel_location.travelLocation_ID")
+     console.log(req.params)
+     var inserts = [req.params.location];
+     console.log(query);
+     mysql.pool.query(query, function (error, results, fields) {
+         if (error) {
+             res.write(JSON.stringify(error));
+             res.end();
+            }
+         context.location = results;
+         complete();
+        });
+    }
+function getGuideDetails(req, res, mysql, context, complete) {
+        var sql = ("SELECT tour_guide.tourGuide_ID AS 'guide_ID," +
+                    "tour_guide.first_name AS 'first_name', tour_guide.last_name AS 'last_name'," +
+                    "COUNT(assignment.tourGuide_travelLocation) AS 'count_assignment'," +
+                    "COUNT(travel_location.travelLocation_ID) AS 'count_locations'" +
+                    "FROM tour_guide" +
+                    "WHERE tour_guide.tourGuide_ID = ISNULL(?, tour_guide.tourGuide_ID)" +
+                    "LEFT JOIN assignment ON tour_guide.tourGuide_ID = assignment.tourGuide_ID" +
+                    "LEFT JOIN travel_location ON assignment.travelLocation_ID = travel_location.travelLocation_ID" +
+                    "GROUP BY tour_guide.tourGuide_ID" +
+                    "ORDER BY tour_guide.tourGuide_ID")
+        console.log(req.params)
+        var inserts = [req.params.location];
+        console.log(query);
+        mysql.pool.query(query, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.location = results;
+            complete();
+        });
+    }
 
 function getPayment(res, mysql, context, complete){
   var sql_query = "SELECT payment.payment_ID, payment.booking_ID, payment.payment_amount, payment.payment_date, payment.payment_description FROM payment LEFT JOIN bookings ON payment.booking_ID = bookings.booking_ID GROUP BY payment.payment_ID ORDER BY payment.payment_ID;";
@@ -100,12 +149,61 @@ function selectLocation(res, mysql, context, complete) {
       complete();
   });
 }
+function getLocations(res, mysql, context, complete) {
+    mysql.pool.query("SELECT travel_location.travelLocation_ID as location_ID," + 
+                    "CONCAT(travel_location.city, ', ', travel_location.country) AS location_name" +
+                    "FROM travel_location",
+        function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.location_display = results;
+            complete();
+        });
+}
 
+function selectLocation(id) {
+    $("#location-selector").val(id);
+}
+function filterLocations() {
+    //get the id of the selected location from the filter dropdown
+        var location_ID = document.getElementById('location_filter').value
+    //        //construct the URL and redirect to it
+                window.location = '/locations/filter/' + parseInt(location_ID)
+}
+function getGuides(res, mysql, context, complete) {
+    mysql.pool.query("SELECT tour_guide.tourGuide_ID AS tour_guide_ID," +
+                    "CONCAT(tour_guide.first_name, ' ', tour_guide.last_name) as guide_full_name" +
+                    "FROM tour_guide",
+        function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.location_display = results;
+            complete();
+        });
+}
+
+function selectGuide(id) {
+    $("#guide-selector").val(id);
+}
+function filterGuide() {
+    //get the id of the selected location from the filter dropdown
+        var guide_ID = document.getElementById('guide_filter').value
+    //        //construct the URL and redirect to it
+                window.location = '/guide/filter/' + parseInt(guide_ID)
+}
 app.get('/', function(req, res, next){
   // Render the homepage.
   res.render('home');
 });
 
+app.get('/home', function(req, res, next) {
+  res.render('home.handlebars');
+});
+ 
 app.get('/customer', function(req, res, next){
   var callbackCount = 0;
   var context = {};
@@ -131,6 +229,35 @@ app.get('/booking', function(req, res, next){
     }
   }
 });
+app.get('/locations', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["scripts.js"];
+        var mysql = req.app.get('mysql');
+        getLocations(res, mysql, context, complete);
+        getLocationsDetails(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('locations', context);
+            }
+        }
+    });
+
+app.get('/tour_guides', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["scripts.js"];
+        var mysql = req.app.get('mysql');
+        getGuides(res, mysql, context, complete);
+        getGuideDetails(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('guides', context);
+            }
+        }
+    });
 
 app.get('/payment', function(req, res, next){
   var callbackCount = 0;
@@ -170,7 +297,6 @@ app.get('/add-rating', function(req, res, next){
     }
   }
 });
-
 app.get('/add-customer', function(req, res) {
   res.render('add-customer.handlebars')
 });
@@ -182,7 +308,7 @@ app.get('/add-payment', function(req, res) {
 });
 app.get('/add-rating', function(req,res) {
   res.render('add-rating.handlebars');
-});
+});	
 
 app.post('/add-customer', function(req, res, next) {
   console.log(req.body)
@@ -216,6 +342,23 @@ console.log(req.body)
   });
 });
 
+app.post('/', function(req, res){
+        console.log(req.body.location)
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO travel_location (city, country) VALUES (?,?)";
+        var inserts = [req.body.city, req.body.country];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/locations', context);
+            }
+        });
+    });
+
 app.post('/add-payment', function(req, res, next){
   console.log(req.body)
   var mysql = req.app.get('mysql');
@@ -231,6 +374,23 @@ app.post('/add-payment', function(req, res, next){
       }
   });
 });
+
+app.post('/', function(req, res){
+        console.log(req.body.location)
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO tour_guide (first_name, last_name) VALUES (?,?)";
+        var inserts = [req.body.first_name, req.body.last_name];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/guides', context);
+            }
+        });
+    });
 
 app.post('/add-rating', function(req, res, next){
   console.log(req.body)
