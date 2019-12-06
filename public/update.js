faa877184d56fec6f132924ceb151da710f8ac21
manickaa <52226049+manickaa@users.noreply.github.com>
2019-12-05 22:38:25 -0800

    Update update.js

--- public/update.js
+++ public/update.js
@@ -19,3 +19,14 @@ function updateLocation(id) {
         }
     })
 };
+
+function updateCustomer(id){
+    $.ajax({
+        url: '/customer/' + id,
+        type: 'PUT',
+        data: $('#update-customer').serialize(),
+        success: function(result){
+            window.location.replace("/customer");
+        }
+    })
+};
